15b40612a43f188d43e734da6cfa19079e0351fb
SamyogGhimire <samyog.ghimire25@gmail.com>
2026-07-21 21:42:16 +0530

    Railway config

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
 const app = require('./app');
 
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`[Startup] Server listening on port ${PORT}`);
+  });
+}
+
 module.exports = app;
